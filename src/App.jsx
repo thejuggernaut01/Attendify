374282edca0f8faa62647c8e5d4d411721d0a732
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-07-21 22:11:37 +0100

    structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([{ path: "" }]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
