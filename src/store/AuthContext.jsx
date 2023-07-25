851520a940994cbe04756cff25fd43b37515789d
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-07-25 20:43:09 +0100

    added login & signUp code to AuthContext

--- src/store/AuthContext.jsx
+++ src/store/AuthContext.jsx
@@ -1,12 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+import { auth } from "../firebase";
 
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState("");
+
   const [loading, setLoading] = useState(false);
   const [startSession, setStartSession] = useState(false);
   const [showSessionDetails, setShowSessionDetails] = useState(false);
   const [showCode, setShowCode] = useState(false);
+
+  useEffect(() => {
+    const subscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      setCurrentUser(user);
+    });
+
+    return subscribe;
+  });
+
+  const signUp = async (email, password) => {
+    await createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const login = async (email, password) => {
+    await signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logout = () => {
+    return signOut(auth);
+  };
 
   const value = {
     startSession,
@@ -15,6 +46,11 @@ export const AuthProvider = ({ children }) => {
     setShowSessionDetails,
     showCode,
     setShowCode,
+
+    signUp,
+    login,
+    logout,
+    currentUser,
   };
 
   return (
