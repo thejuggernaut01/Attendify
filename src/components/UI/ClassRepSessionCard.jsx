125eab2df9ebe02a96c7ed073ed7340536294b4c
devhabeeblateef <habeeblateef002@gmail.com>
2023-08-09 22:24:17 +0100

    complete scanner

--- src/components/UI/ClassRepSessionCard.jsx
+++ src/components/UI/ClassRepSessionCard.jsx
@@ -4,13 +4,9 @@ import { powerSvg } from "./Svg";
 import { db } from "../../firebase";
 import { collection, addDoc } from "firebase/firestore";
 import { AuthContext } from "../../store/AuthContext";
-import { QrReader } from "react-qr-reader";
 
 const ClassRepSessionCard = () => {
   const { startSession, setStartSession, setShowSessionDetails, setShowCode } = useContext(AuthContext);
-  const [isScanning, setIsScanning] = useState(false);
-  const [delay, setDelay] = useState(100);
-  const [result, setResult] = useState("No Result");
 
   const startSessionHandler = () => {
     setShowSessionDetails((prevState) => !prevState);
@@ -21,86 +17,28 @@ const ClassRepSessionCard = () => {
     setShowCode((prevState) => !prevState);
   };
 
-  const handleScan = async (data) => {
-    if (data) {
-      // Barcode/QR code data is available in 'data'
-      setResult(data); // Store the scanned data in 'result' state
-      setIsScanning(false); // Stop scanning
-      await saveAttendance(data); // Save the scanned data to Firestore
-    }
-  };
-
-  const handleError = (err) => {
-    console.error(err);
-  };
-
-  const toggleScanner = () => {
-    setIsScanning((prev) => !prev);
-    setResult("No Result"); // Clear the previous scanned data when starting a new scan
-  };
-
-  // Save attendance
-  const saveAttendance = async (data) => {
-    const dataToSave = {
-      qrData: data,
-      timestamp: new Date().toISOString(), // You can include a timestamp if needed
-    };
-
-    try {
-      await saveDataToFirestore(dataToSave);
-      console.log("Data saved:", dataToSave);
-    } catch (error) {
-      console.error("Error saving data:", error);
-    }
-  };
-
-  const saveDataToFirestore = (data) => {
-    return addDoc(collection(db, "attendanceCollection"), data);
-  };
-
-  const previewStyle = {
-    height: 240,
-    width: 320,
-  };
 
   return (
     <>
       <section className="mt-5">
         <div className="text-center bg-white rounded-2xl py-6 w-[90%] sm:w-[70%] mx-auto">
           <div className="flex justify-center">
-            {startSession ? (
+            {startSession ? 
               <button className="text-red-500" onClick={endSessionHandler}>
                 {powerSvg}
               </button>
-            ) : (
-              <>
-                {isScanning ? (
-                  <button className="text-red-500" onClick={toggleScanner}>
-                    Stop Scanning
-                  </button>
-                ) : (
-                  <button className="text-green-500" onClick={toggleScanner}>
+              :  
+               <button onClick={startSessionHandler} className="text-green-500">
                     {powerSvg}
                   </button>
-                )}
-              </>
-            )}
+                }
+              
+            
           </div>
           {startSession ? (
             <p className="text-lg font-bold italic">End session</p>
           ) : (
             <p className="text-lg font-bold italic">Start a session</p>
-          )}
-          {isScanning && (
-            <div>
-              <QrReader
-                delay={delay}
-                style={previewStyle}
-                onError={handleError}
-                onScan={handleScan}
-              />
-              {result !== "No Result" && <p>{result}</p>} {/* Display the scanned data from 'result' state */}
-            </div>
           )}
         </div>
       </section>
--- src/components/UI/ClassRepSessionDetails.jsx
+++ src/components/UI/ClassRepSessionDetails.jsx
@@ -1,8 +1,36 @@
+import React, { useState, useEffect, useRef } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../store/AuthContext";
 import { qrCode } from "./Svg";
+import QrScanner from 'qr-scanner';
 
 const ClassRepSessionDetails = ({ data }) => {
+  QrScanner.WORKER_PATH = '/qrscanner.min.js';
+
+  const [scanning, setScanning] = useState(false);
+  const [scannerResults, setScannerResults] = useState([]);
+  const [successMessage, setSuccessMessage] = useState("");
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const videoElem = videoRef.current;
+
+    if (videoElem && scanning) {
+      const qrScanner = new QrScanner(videoElem, result => {
+        setScannerResults(prevResults => [...prevResults, result]);
+        qrScanner.stop();
+        setSuccessMessage("Attendance Recorded Successfully");
+        setScanning(false);
+      });
+
+      qrScanner.start();
+
+      return () => {
+        qrScanner.destroy();
+      };
+    }
+  }, [scanning]);
+
   const {
     showSessionDetails,
     setShowSessionDetails,
@@ -11,13 +39,30 @@ const ClassRepSessionDetails = ({ data }) => {
     setShowCode,
   } = useContext(AuthContext);
 
-  const beginSessionHandler = () => {
+  const beginSessionHandler = (e) => {
+    e.preventDefault();
     setShowSessionDetails((prevState) => !prevState);
     setStartSession((prevState) => !prevState);
     setShowCode((prevState) => !prevState);
   };
 
-  // console.log(data);
+  const scanHandler = () => {
+    setScanning(true);
+    setSuccessMessage("");
+  };
+
+  const addAnotherStudent = () => {
+    setScanning(true);
+    setSuccessMessage("");
+  };
+  
+  // Check Scanned Student List
+//  useEffect(() => {
+//   if (scannerResults != ""){
+//     console.log(scannerResults)
+//   }
+//  }, [scannerResults])
+
 
   return (
     <>
@@ -28,13 +73,13 @@ const ClassRepSessionDetails = ({ data }) => {
             style={{ backgroundColor: "#101218" }}
           >
             <h3 className="text-3xl">Session Details</h3>
-
+          <form className="text-center w-64" onSubmit={beginSessionHandler}>
             <select
-              className="text-[#b7b7b7] mt-7 px-16 py-2 border border-white rounded-full appearance-none outline-white text-center bg-black"
+              className="text-[#b7b7b7] text-center mt-7 px-16 py-2 border border-white rounded-full appearance-none outline-white m-auto bg-black"
               name="course"
               id="course"
-            >
-              <option className="text-left" value="">
+            required>
+              <option className="" value="">
                 Choose Course
               </option>
               {data.map((data) => (
@@ -48,20 +93,16 @@ const ClassRepSessionDetails = ({ data }) => {
               ))}
             </select>
 
-            <button className="text-[#b7b7b7] mt-5 px-[5.3rem] py-2 border border-white rounded-full appearance-none outline-white text-left bg-black">
-              Start Time
-            </button>
+            <input placeholder="Start Time" type="text"  className="bg-black rounded-full w-64 mt-4 text-center py-2 text-[#B7B7B7] border" required/>
 
-            <button className="text-[#b7b7b7] mt-5 px-[5.3rem] py-2 border border-white rounded-full appearance-none outline-white text-left bg-black">
-              Stop Time
-            </button>
-
-            <p
-              onClick={beginSessionHandler}
-              className="mt-8 border-b-4 cursor-pointer"
+            <input type="text" placeholder="End Time" className="bg-black rounded-full w-64 mt-4 text-center py-2 text-[#B7B7B7] border" required/>
+            <button
+            type="submit"
+              className="text-center mt-8 border-b-4 cursor-pointer"
             >
               Begin Session
-            </p>
+            </button>
+            </form>
           </div>
         ) : !showCode ? (
           <div className="flex justify-center items-center bg-white rounded-2xl py-6 w-[90%] sm:w-[70%] h-80 mx-auto">
@@ -69,8 +110,26 @@ const ClassRepSessionDetails = ({ data }) => {
           </div>
         ) : showCode ? (
           <div className="flex justify-center items-center flex-col bg-white rounded-2xl py-6 w-[90%] sm:w-[70%] h-80 mx-auto">
-            <p>{qrCode}</p>
-            <p>Click to scan</p>
+            {scanning ? 
+              // QRScanner
+              <>
+              <video ref={videoRef} className="w-full max-w-md" />
+              <p className="text-center text-green-800">{successMessage}</p>
+              <button
+                onClick={addAnotherStudent}
+                className="text-center mt-2 border-b-4 cursor-pointer"
+              >
+                Add Another Student
+              </button>
+              </>
+              
+              : 
+              <div>
+              <p onClick={scanHandler}>{qrCode}</p> 
+              </div>
+            }
+         
+            
           </div>
         ) : (
           ""
